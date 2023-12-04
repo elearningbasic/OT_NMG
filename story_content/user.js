93ad15f732a89c50d0cc0ae79f6b9fef50aec8f2
elearningbasic <elearning.basic@gmail.com>
2023-12-04 23:35:53 +0300

    1

---
+++ story_content/user.js
@@ -0,0 +1,51 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6mrUyQ1TDt7":
+        Script1();
+        break;
+      case "69tSeJJRXN5":
+        Script2();
+        break;
+      case "5z2xEFx2P3D":
+        Script3();
+        break;
+      case "5j8xPnsONVB":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script2()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script3()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script4()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
